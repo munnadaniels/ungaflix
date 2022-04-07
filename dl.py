aefe6a836acfe9188ae2307dcb8bad43af816c10
Vanajakshi <84600096+munnadaniels@users.noreply.github.com>
2022-04-07 22:38:38 +0530

    Update dl.py

--- dl.py
+++ dl.py
@@ -99,7 +99,7 @@ def merge_content():
 def divider():
         FILENAME= args.output
         output =  OUTPUT_PATH + '/' + f"{FILENAME}"
-        logo =  logos + '/' + f"{troop.png}"
+        logo =  logos + '/' + f"{FILENAME}"
         os.system('ffmpeg -i output -i logo -filter_complex "[0:v][1:v] overlay=25:25:enable=between(t,0,20)" FILENAME copy %s/%s'%(TEMPORARY_PATH,TEMPORARY_PATH,OUTPUT_PATH,FILENAME))
 
 def rclone():
