d9b502aa2d8f1e44fd933f940970a681c3d92980
Vanajakshi <84600096+munnadaniels@users.noreply.github.com>
2022-05-04 19:08:24 +0530

    Update jio.py

--- jio.py
+++ jio.py
@@ -148,11 +148,11 @@ def get_streams(m3u8):
     print ("Downloading A/V")
     os.system(f'{ytdl_path} {m3u8} --allow-unplayable-formats --external-downloader aria2c --user-agent "JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio" -q --no-warnings') # + -P TEMP:{cachePath} -P HOME:{outputpath/fileName}
     if args.res == 'low':
-         os.rename(f'playlist_HD_TV_L [playlist_HD_TV_L].mp4', fileName)
+         os.rename(f'playlist_HD_TV_L [playlist_HD_TV_L].mp4', OUTPUT_PATH/fileName)
     elif args.res == 'med':
-         os.rename(f'playlist_HD_TV_M [playlist_HD_TV_M].mp4', fileName)
+         os.rename(f'playlist_HD_TV_M [playlist_HD_TV_M].mp4', OUTPUT_PATH/fileName)
     elif args.res == 'high':
-         os.rename(f'playlist_HD_TV_H [playlist_HD_TV_H].mp4', fileName)
+         os.rename(f'playlist_HD_TV_H [playlist_HD_TV_H].mp4', OUTPUT_PATH/fileName)
     else:
          os.rename(f'chunklist [chunklist].mp4', OUTPUT_PATH/fileName)
     print ("\nSuccessfully downloaded the stream!") 
