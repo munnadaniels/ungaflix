0f47dcbdce385fe311574538415f06ceb0041ba3
Vanajakshi <84600096+munnadaniels@users.noreply.github.com>
2022-05-08 03:27:35 +0530

    Update js.py

--- js.py
+++ js.py
@@ -49,7 +49,7 @@ def get_metadata(VideoID):
            AUDIO_ID = "ba"
     divider()
     os.system(f'yt-dlp --no-warnings --external-downloader aria2c --allow-unplayable-formats --user-agent "JioOnDemand/1.5.2.1 (Linux;Android 4.4.2)" -f {VIDEO_ID} "{m3u8}"')
-    os.rename(f'cache/chunklist [chunklist].mp4', outputt)
+    os.rename(f'chunklist [chunklist].mp4', outputt)
     #print ("\nSuccessfully downloaded the stream!") 
 
 def subtitles(m3u8):
