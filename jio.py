210ceadae72d5bace70dfad290ccad3d76c0a0a7
Vanajakshi <84600096+munnadaniels@users.noreply.github.com>
2022-05-08 02:20:17 +0530

    Update jio.py

--- jio.py
+++ jio.py
@@ -148,7 +148,7 @@ def get_streams(m3u8):
            AUDIO_ID = "ba"
     divider()
     os.system(f'yt-dlp --no-warnings --external-downloader aria2c --allow-unplayable-formats --user-agent "JioOnDemand/1.5.2.1 (Linux;Android 4.4.2)" -f {VIDEO_ID} "{m3u8}"')
-    os.rename(f'ungak', output)
+    os.rename(f'%s', output''%ungak))
     #print ("\nSuccessfully downloaded the stream!") 
 
 def trackname():
