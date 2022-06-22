fc29c9748e294c557080f860e802950b3f8053ef
Vanajakshi <84600096+munnadaniels@users.noreply.github.com>
2022-06-22 22:14:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,1 @@
-import os
-
-w = open('config.json', 'w+')
-w.write('{')
-w.write('\n')
-w.write('    "authToken": "'+os.getenv('BOT_TOKEN')+'",')
-w.write('\n')
-w.write('    "owner": '+os.getenv('OWNER_ID'))
-w.write('\n')
-w.write('}')
+{ "authToken": "5100078702:AAHveHDTRCubIg-ALCm93QDKeVANF1V2BWs", "owner": 1807485738}
